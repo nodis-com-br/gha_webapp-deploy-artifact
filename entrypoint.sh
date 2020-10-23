c768f2aabcd59e2b7101dd4f3e43e3a4d0a70ada
Pedro Tonini <pedro.tonini@nodis.com.br>
2020-10-23 14:45:31 -0300

    refactoring

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,7 @@ set -e
 [[ ${NODIS_DEPLOY_ENV} == "qa" && ${DEPLOY_QA_TO_PROD} != "false" ]] && NODIS_DEPLOY_ENV="prod"
 [[ ${NODIS_DEPLOY_ENV} == "qa" && ${DEPLOY_QA_TO_DEV} == "true" ]] && NODIS_DEPLOY_ENV="dev"
 
-DISTRIBUTION_ID=`aws cloudfront list-distributions --query 'DistributionList.Items[?(Origins.Items[0].Id==`s3-'${NODIS_WEBAPP_BUCKET}'`)&&(Origins.Items[0].OriginPath==`/'${NODIS_DEPLOY_ENV}'`)].Id' --output text`
+DISTRIBUTION_ID=$(aws cloudfront list-distributions --query 'DistributionList.Items[?(Origins.Items[0].Id==`s3-'${NODIS_WEBAPP_BUCKET}'`)&&(Origins.Items[0].OriginPath==`/'${NODIS_DEPLOY_ENV}'`)].Id' --output text)
 
 aws s3 cp s3://${NODIS_ARTIFACT_BUCKET}/${NODIS_PROJECT_NAME}/${NODIS_ARTIFACT_FILENAME} .
 
